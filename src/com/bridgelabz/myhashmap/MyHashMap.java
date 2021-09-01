71e88194c41838b6ac6ccf5c9652e5d40a71436a
shruthishruu05 <shruthi.m@perfios.com>
2021-09-01 21:13:45 +0530

    ADDED : Contains hashmap operations

--- src/com/bridgelabz/myhashmap/MyHashMap.java
+++ src/com/bridgelabz/myhashmap/MyHashMap.java
@@ -1,30 +1,53 @@
 package com.bridgelabz.myhashmap;
 import com.bridgelabz.linkedlist.*;
 
-public class MyHashMap<K extends Comparable<K>, V extends Comparable<V>> {
+import java.util.ArrayList;
 
-	LinkedList<K> hashLinkedList;
-
+public class MyHashMap<K extends Comparable<K>,V extends Comparable <V>> {
+	private final int numBuckets; 
+	ArrayList<LinkedList<K>> myBucketArray; 
+	
 	public MyHashMap() {
-		this.hashLinkedList = new LinkedList<>();
+		this.numBuckets = 10;
+		this.myBucketArray = new ArrayList<LinkedList<K>>(numBuckets);
+		for(int i=0; i< numBuckets; i++) this.myBucketArray.add(null);
 	}
-
+	
+	private int getBucketIndex(K key) {
+		int hashCode = Math.abs(key.hashCode());
+		int index = hashCode% numBuckets;
+		return index;
+	}
+	
 	public V get(K key) {
-		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.hashLinkedList.search(key);
+		int index = getBucketIndex(key);
+		LinkedList<K> myLinkedList = myBucketArray.get(index);
+		if(myLinkedList == null) return null;
+		MyMapNode<K,V> myMapNode = (MyMapNode<K,V>) myLinkedList.search(key);
 		return (myMapNode == null) ? null : myMapNode.getValue();
 	}
-
+	
 	public void add(K key, V value) {
-		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.hashLinkedList.search(key);
-		if (myMapNode == null) {
-			myMapNode = new MyMapNode<K, V>(key, value);
-			this.hashLinkedList.appendNodes(myMapNode);
-		} else {
+		int index = getBucketIndex(key);
+		LinkedList<K> LinkedList = myBucketArray.get(index);
+		if(LinkedList == null) {
+			LinkedList = new LinkedList<K>();
+			this.myBucketArray.set(index,LinkedList);
+		}
+		MyMapNode<K,V> myMapNode = (MyMapNode<K,V> ) LinkedList.search(key);
+		if(myMapNode == null) {
+			myMapNode = new MyMapNode<K,V> (key, value);
+			LinkedList.append(myMapNode);
+		}
+		else {
 			myMapNode.setValue(value);
 		}
 	}
+	
 	@Override
 	public String toString() {
-		return "Hash Map{" + hashLinkedList + "}";
+		
+		return "MyHashMapNodes{" +myBucketArray+ "}";
 	}
+	
 }
