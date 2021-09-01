338d3ca1393dc4949647631cf28def3a7129098b
shruthishruu05 <shruthi.m@perfios.com>
2021-09-01 21:14:36 +0530

    ADDED : hashmap main method

--- src/com/bridgelabz/myhashmap/MyHashMapMain.java
+++ src/com/bridgelabz/myhashmap/MyHashMapMain.java
@@ -4,25 +4,18 @@ public class MyHashMapMain {
 
 	public static void main(String[] args) {
 		
-		System.out.println("----- Welcome To Data Structure: HashMap -----");
-		
-		String sentence = "To be or not to be";
-		MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
-		String[] words = sentence.toLowerCase().split(" ");
-		
-		for(String word : words) {
-			Integer value = myHashMap.get(word);
-			if(value == null)
-				value = 1;
-			else 
-				value = value+1;
-			myHashMap.add(word, value);
+			String sentence = "Paranoids are not paranoid because they are paranoid but "+"beacuse they keep putting themselves deliberately into "
+								+"paranoid avoidable situations";
+			MyHashMap<String, Integer> myHashMap = new MyHashMap<String, Integer>();
+			String [] words = sentence.toLowerCase().split(" ");	
+			for( String word : words) {
+				Integer value = myHashMap.get(word);
+				if(value == null) value = 1;
+				else value  = value+1;
+				myHashMap.add(word, value);
+			}
+			Integer frequency  = myHashMap.get("paranoid");
+			System.out.println(myHashMap);
+			System.out.println("the word \"paranoid\" is repeated :"+frequency+" times");
 		}
-		System.out.println(myHashMap);
-		
-		for (String word:words) {
-			int frequency = myHashMap.get(word);
-			System.out.println("Frequency of "+word+" : "+frequency);
-		}
-	}
 }
